dffc66365a41131b4787df9a8d1b7191b70b99ac
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2022-11-21 14:18:11 +0100

    main.tf to set up bigip provider

---
+++ terraformer/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    bigip = {
+      source = "F5Networks/bigip"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "bigip" {
+  address  = var.hostname
+  username = var.username
+  password = var.password
+}
